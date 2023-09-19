49b37695f3a09a67ab050fc991dab07e56b9b8ef
jaggyz <jaggyzmail@yahoo.com>
2023-09-19 21:19:07 +0700

    [Edit] test script

--- royalty_point_test.go
+++ royalty_point_test.go
@@ -2,9 +2,9 @@ package royaltyPoint
 
 import "testing"
 
-func TestPointShouldBe0WhenPriceIs80(t *testing.T) {
-	expected := 0
-	if actual := point(80.00); actual != expected {
+func TestPointShouldBe_WhenPriceIs_(t *testing.T) {
+	expected := 
+	if actual := point(); actual != expected {
 		t.Errorf("expected %d, actual %d", expected, actual)
 	}
 }
