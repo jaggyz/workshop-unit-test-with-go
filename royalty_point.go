22479c64613b37f96f7a6a124692dcba695e02b1
jaggyz <jaggyzmail@yahoo.com>
2023-09-19 21:16:06 +0700

    [Edit] function point

--- royalty_point.go
+++ royalty_point.go
@@ -1,5 +1,20 @@
 package royaltyPoint
 
+//make it work
 func point(price float64) int {
-	return int(price / 100)
+	return 0
 }
+
+//make it work
+// func point(price float64) int {
+// 	if price < 100.00 {
+// 		return 0
+// 	} else {
+// 		return 1
+// 	}
+// }
+
+//make it right
+// func point(price float64) int {
+// 	return int(price / 100)
+// }
